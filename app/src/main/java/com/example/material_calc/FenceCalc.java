b8faff1f55e43e92537abc1878a580175e01389b
pcgofpa <pcgofpa@gmail.com>
2022-03-01 13:06:26 -0600

    Added Home Button to return to main Activity from Fence Calculator

--- app/src/main/java/com/example/material_calc/FenceCalc.java
+++ app/src/main/java/com/example/material_calc/FenceCalc.java
@@ -2,7 +2,10 @@ package com.example.material_calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FenceCalc extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class FenceCalc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fence_calc);
+
+        //Go to FenceCalc Activity
+        Button Home = (Button) findViewById(R.id.Home);
+        Home.setText("Home");
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FenceCalc.this, MainActivity.class));
+            }
+        });
     }
 }
