c9c8a6abce7cbebe1f6d787d0d897f86bdcf4e51
Phil <pcgofpa@gmail.com>
2022-03-01 03:03:12 -0600

    Created Fence calc activity, flooring activity, modified background for main.xml

---
+++ app/src/main/java/com/example/material_calc/Flooring.java
@@ -0,0 +1,14 @@
+package com.example.material_calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Flooring extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flooring);
+    }
+}
