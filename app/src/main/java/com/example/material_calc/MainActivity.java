3ce61f76c8ba3f388630ec4f9a0abaf5111e36e8
pcgofpa <pcgofpa@gmail.com>
2022-03-01 13:13:52 -0600

    Set onClickListener to Transition between Home Screen and Fence Calc Activity

--- app/src/main/java/com/example/material_calc/MainActivity.java
+++ app/src/main/java/com/example/material_calc/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.material_calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set OnClick Listener to Move to Fence Calculator Activity
+        Button Fence = (Button) findViewById(R.id.Fence);
+        Fence.setText("Fence Calculator");
+        Fence.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FenceCalc.class));
+            }
+        });
     }
 }
